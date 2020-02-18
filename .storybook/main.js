557444df0239d5c7fcb09cfe0ec3bf898d80287a
Noerani Samsoedien <n.samsoedien@payter.nl>
2020-02-18 17:26:41 +0100

    chore: setup storybook

---
+++ .storybook/main.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+  stories: ['../packages/**/*.stories.(ts|tsx|js|jsx)'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [['react-app', { flow: false, typescript: true }]],
+          },
+        },
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+          options: {
+            tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
+          },
+        },
+      ],
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  },
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
+    '@storybook/addon-storysource',
+    '@storybook/addon-a11y',
+    '@storybook/addon-jest',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+  ],
+}
