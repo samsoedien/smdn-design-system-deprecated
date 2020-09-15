fd69a82118c95459ec8c8b85beae2489555993d5
Samsoedien <contact@samsoedien.com>
2020-09-15 21:47:08 +0200

    chore(storybook): upgrade to storybook 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,40 +1,11 @@
-const path = require('path')
-
 module.exports = {
-  stories: ['../packages/**/*.stories.(ts|tsx|js|jsx|mdx)'],
+  stories: ['../stories/**/*.stories.mdx', '../packages/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
   addons: [
-    '@storybook/addon-actions',
     '@storybook/addon-links',
+    '@storybook/addon-essentials',
     '@storybook/addon-knobs',
     '@storybook/addon-storysource',
     '@storybook/addon-a11y',
     '@storybook/addon-jest',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-      },
-    },
   ],
-  webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [['react-app', { flow: false, typescript: true }]],
-          },
-        },
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-          options: {
-            tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
-          },
-        },
-      ],
-    })
-    config.resolve.extensions.push('.ts', '.tsx')
-    return config
-  },
 }
